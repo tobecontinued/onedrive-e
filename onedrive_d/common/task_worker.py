2fdfda036e338d022ae7d0dc6b552218c6d55bc2
Xiangyu Bu <xybu92@live.com>
2015-08-29 21:25:15 -0900

    TaskWorker thread model

---
+++ onedrive_d/common/task_worker.py
@@ -0,0 +1,31 @@
+__author__ = 'xb'
+
+import threading
+
+from onedrive_d.common import logger_factory
+
+
+class TaskConsumer(threading.Thread):
+    terminate_sign = threading.Event()
+    logger = logger_factory.get_logger('TaskConsumer')
+
+    def __init__(self, task_pool):
+        """
+        :param onedrive_d.store.task_pool.TaskPool task_pool:
+        """
+        super().__init__()
+        self.daemon = True
+        self.task_pool = task_pool
+
+    def run(self):
+        while True:
+            self.task_pool.semaphore.acquire()
+            if self.terminate_sign.is_set():
+                return
+            task = self.task_pool.pop_task()
+            self.logger.debug('Acquired task of type "%s" on path "%s"',
+                              task.__class__.__name__, task.local_parent_path + '/' + task.name)
+            task.handle()
+
+
+TaskConsumer.terminate_sign.clear()
