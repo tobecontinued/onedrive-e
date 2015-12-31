841d20f55ff0f6c1a963b78e3890b517738c984b
Xiangyu Bu <xybu92@live.com>
2015-12-30 17:26:29 -0800

    Remove delay design in DeleteItemTask.

--- onedrive_d/common/tasks/delete_task.py
+++ onedrive_d/common/tasks/delete_task.py
@@ -1,51 +1,23 @@
-from time import sleep
-
 from onedrive_d.api import errors
 from onedrive_d.common.tasks import TaskBase
 
 
 class DeleteItemTask(TaskBase):
-    def __init__(self, parent_task, rel_parent_path, item_name, is_folder, pause_sec=0):
+    def __init__(self, parent_task, rel_parent_path, item_name, is_folder):
         """
         :param TaskBase parent_task: Base task.
         :param str rel_parent_path: Relative working path of this task.
         :param str item_name: Name of the item to deal with.
         :param True | False is_folder: True to indicate the task is on a folder.
-        :param int pause_sec: Time, in seconds, to wait before executing the task.
         """
         super().__init__(parent_task)
         self.rel_parent_path = rel_parent_path
         self.item_name = item_name
         self.is_folder = is_folder
-        self.pause_sec = pause_sec
-        self._handled = False
-        self.handling = False
-
-    @property
-    def handled(self):
-        return self._handled
-
-    def set_handled(self):
-        """
-        MoveItemTask couldl set a delayed deletion task to "handled" status so that the latter can be skipped.
-        """
-        self._handled = True
-        self.task_pool.mark_handled(self)
-        self.logger.info('Delayed deletion task on "%s" has been handled externally.', self.local_path)
 
     def handle(self):
-        self.handling = True
-        if not self.handled and self.pause_sec > 0:
-            try:
-                sleep(self.pause_sec)
-            except:
-                pass
-        if self.handled:
-            return
         try:
             self.drive.delete_item(item_path=self.remote_path)
-            if self.pause_sec > 0:
-                self.task_pool.mark_handled(self)
             self.items_store.delete_item(parent_path=self.remote_parent_path, item_name=self.item_name,
                                          is_folder=self.is_folder)
             self.logger.info('Deleted entry "%s".', self.local_path)
--- onedrive_d/store/task_pool.py
+++ onedrive_d/store/task_pool.py
@@ -15,7 +15,6 @@ class TaskPool:
     def __init__(self):
         self.tasks_by_path = {}
         self.queued_tasks = []
-        self.unhandled_tasks = []
         self.semaphore = threading.Semaphore(0)
         self._lock = threading.Lock()
 
@@ -26,10 +25,7 @@ class TaskPool:
         """
         self._lock.acquire()
         self.queued_tasks.append(task)
-        if task.local_path not in self.tasks_by_path:
-            self.tasks_by_path[task.local_path] = [task]
-        else:
-            self.tasks_by_path[task.local_path].append(task)
+        self.tasks_by_path[task.local_path] = task
         self._lock.release()
         self.semaphore.release()
 
@@ -51,52 +47,16 @@ class TaskPool:
                         self.queued_tasks.remove(t)
                         break
         if ret is not None:
-            self.tasks_by_path[ret.local_path].remove(ret)
-            # Handle delayed tasks.
-            if hasattr(ret, 'handled') and hasattr(ret, 'pause_sec') and ret.pause_sec > 0:
-                self.unhandled_tasks.append(ret)
+            del self.tasks_by_path[ret.local_path]
         self._lock.release()
         return ret
-
-    def get_task_by_local_path(self, local_path, task_class=None):
-        self._lock.acquire()
-        if local_path in self.tasks_by_path:
-            if task_class is None:
-                ret = self.tasks_by_path[local_path].copy()
-            else:
-                ret = [t for t in self.tasks_by_path[local_path] if isinstance(t, task_class)]
-        else:
-            ret = []
-        if len(self.unhandled_tasks) > 0:
-            if task_class is None:
-                ret += [t for t in self.unhandled_tasks if t.local_path == local_path]
-            else:
-                ret += [t for t in self.unhandled_tasks if isinstance(t, task_class) and t.local_path == local_path]
-        self._lock.release()
-        return ret
-
-    def delete_task(self, task):
-        self._lock.acquire()
-        if task.local_path in self.tasks_by_path and task in self.tasks_by_path[task.local_path]:
-            self.tasks_by_path[task.local_path].remove(task)
-        if task in self.unhandled_tasks:
-            self.unhandled_tasks.remove(task)
-        if task in self.queued_tasks:
-            self.queued_tasks.remove(task)
-        self._lock.release()
 
     def has_pending_task(self, local_path):
         # FIXME: this function should have been atomic with any subsequent add / pop call.
         self._lock.acquire()
-        ret = local_path in self.tasks_by_path and len(self.tasks_by_path[local_path]) > 0
+        ret = local_path in self.tasks_by_path
         self._lock.release()
         return ret
-
-    def mark_handled(self, task):
-        self._lock.acquire()
-        if task in self.unhandled_tasks:
-            self.unhandled_tasks.remove(task)
-        self._lock.release()
 
     def remove_children_tasks(self, local_parent_path):
         to_be_deleted = []
@@ -104,7 +64,6 @@ class TaskPool:
         for t in self.queued_tasks:
             if t.local_path.startswith(local_parent_path):
                 to_be_deleted.append(t)
-                self.tasks_by_path[t.local_path].remove(t)
+                del self.tasks_by_path[t.local_path]
         self.queued_tasks -= to_be_deleted
-        self.unhandled_tasks -= to_be_deleted
         self._lock.release()
