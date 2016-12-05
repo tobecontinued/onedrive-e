dbcfeb17a5bd89dccecd8f1ba7d0bdd90b3c1d45
Deng Wenqi <tbc.dengwenqi@gmail.com>
2016-12-05 13:48:03 +0800

    when set sha1 as default argument, function generates diff sha1 value for same file in different times. a python bug?

--- onedrived/common/hasher.py
+++ onedrived/common/hasher.py
@@ -1,8 +1,9 @@
 import hashlib
 import zlib
+import sys
 
 
-def hash_value(file_path, block_size=1048576, algorithm=hashlib.sha1()):
+def hash_value(file_path, block_size=1048576, algorithm=None):
     """
     Calculate the MD5 or SHA hash value of the data of the specified file.
     :param str file_path:
@@ -10,6 +11,8 @@ def hash_value(file_path, block_size=1048576, algorithm=hashlib.sha1()):
     :param algorithm: A hash function defined in hashlib.
     :return str:
     """
+    if algorithm is None:
+      algorithm=hashlib.sha1()
     with open(file_path, 'rb') as f:
         while True:
             data = f.read(block_size)
@@ -18,10 +21,12 @@ def hash_value(file_path, block_size=1048576, algorithm=hashlib.sha1()):
             algorithm.update(data)
     return algorithm.hexdigest().upper()
 
-
 def crc32_value(file_path, block_size=1048576):
     """
     Calculate the CRC32 value of the data of the specified file.
+    NOTE: this function's result equals to other software generated.
+    but not equals to the value got by onedrive.
+    So we should use this function in our system.
     :param str file_path:
     :param int block_size:
     :return int:
@@ -33,4 +38,4 @@ def crc32_value(file_path, block_size=1048576):
             if not data:
                 break
             crc = zlib.crc32(data, crc)
-    return str(crc).upper()
+    return format(crc & 0xFFFFFFFF, '08x').upper()
