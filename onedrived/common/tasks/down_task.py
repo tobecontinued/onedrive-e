7b5959379fe0e4af7dbb812d1ced49648598180e
Deng Wenqi <tbc.dengwenqi@gmail.com>
2016-12-05 13:55:28 +0800

    check correctness after downloading file

--- onedrived/common/tasks/down_task.py
+++ onedrived/common/tasks/down_task.py
@@ -3,6 +3,7 @@ import traceback
 
 from onedrived import OS_USER_ID, OS_USER_GID
 from onedrived.api import errors
+from onedrived.common import hasher
 from onedrived.common.dateparser import datetime_to_timestamp
 from onedrived.common.tasks import TaskBase
 from onedrived.store.items_db import ItemRecordStatuses
@@ -29,6 +30,17 @@ class DownloadFileTask(TaskBase):
         try:
             with open(local_item_tmp_path, 'wb') as f:
                 self.drive.download_file(file=f, size=self._item.size, item_id=self._item.id)
+            local_sha1 =  hasher.hash_value(local_item_tmp_path)
+            item_sha1 = None
+            if self._item.file_props is not None and self._item.file_props.hashes is not None:
+                item_sha1 = self._item.file_props.hashes.sha1
+            if item_sha1 is None:
+                self.logger.warn('Remote file %s has not sha1 property, we keep the file but cannot check correctness of it',
+                      self.local_path)
+            elif local_sha1 != item_sha1:
+                self.logger.error('Mismatch hash of download file %s : remote:%s,%d  local:%s %d', self.local_path,
+                     self._item.file_props.hashes.sha1, self._item.size, local_sha1, os.path.getsize(local_item_tmp_path))
+                return 
             os.rename(local_item_tmp_path, self.local_path)
             t = datetime_to_timestamp(self._item.modified_time)
             os.utime(self.local_path, (t, t))
